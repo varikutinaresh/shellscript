86a36388ace0058ec451a7228b500e2c48311a63
varikutinaresh <v.n.rreddy00@gmail.com>
2025-02-19 14:16:42 +0530

    adding 02-vairable.sh,

---
+++ 02-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# this line is commented
+
+echo "ramesh":: "hi suresh"
+echo "suresh":: "hi ramesh"
+echo "ramesh":: "how are you?"
+echo "suresh":: "i am fine and you?"
+echo "ramesh":: "i iam also fine"
