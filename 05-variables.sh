a2753872d650a8636c0d17c8a3c003cc9bef37b9
varikutinaresh <v.n.rreddy00@gmail.com>
2025-02-24 14:58:51 +0530

    modified script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "please enter username::"
+echo "please enter username::" # text entered here will be added as value to varaible
 
-read -s USERNAME
+read -s USERNAME # Here USERNAME is the variable name
 
-echo "username entered: $USERNAME"
+echo "username entered: $USERNAME" 
 
-echo "please enter password::"
+echo "please enter password::" # text entered here will be added as value to varaible
 
-read -s PASSWORD
+read -s PASSWORD # Here PASSWORD is the variable name
 
-echo "password entered: $PASSWORD"
+echo "password entered: $PASSWORD" # we can't print the PASSWORD like this just i added here for practice
