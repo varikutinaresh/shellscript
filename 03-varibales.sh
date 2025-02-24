c176d13a46db7daad7d3ed0abba5a43359592fdf
varikutinaresh <v.n.rreddy00@gmail.com>
2025-02-24 12:49:54 +0530

    conversation added

---
+++ 03-varibales.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=NARESH
+PERSON2=MAHESH
+
+echo "$PERSON1":: "hi $PERSON2"
+echo "$PERSON2":: "hi $PERSON1"
+echo "$PERSON1":: "how are you?"
+echo "$PERSON2":: "i am fine and you?"
+echo "$PERSON1":: "i iam also fine"
