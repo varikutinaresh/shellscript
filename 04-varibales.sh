37f4c2ee7e54477a4f2eb512a16c2fb7c0eb73f2
varikutinaresh <v.n.rreddy00@gmail.com>
2025-02-24 13:13:03 +0530

    added another file

---
+++ 04-varibales.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1":: "hi $PERSON2"
+echo "$PERSON2":: "hi $PERSON1"
+echo "$PERSON1":: "how are you?"
+echo "$PERSON2":: "i am fine and you?"
+echo "$PERSON1":: "i iam also fine"
